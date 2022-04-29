98f962acf375e8b37d151f0cb72c3fb84dec6cd5
Casca Kwok <casca.kwok@gmail.com>
2022-04-29 00:33:46 -0700

    Update test.py

--- Project 6/test.py
+++ Project 6/test.py
@@ -7,19 +7,6 @@ import torchvision
 import sys
 import matplotlib.pyplot as plt
 
-'''
-def mnist_data():
-    compose = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((.5, ), (.5, ))
-        ])
-    out_dir = './dataset'
-    return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)# Load data
-data = mnist_data()# Create loader with data, so that we can iterate over it
-data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
-# Num batches
-num_batches = len(data_loader)
-'''
 
 def mnist_data(batch_size_train):
     #------ Download Training and Testing Dataset.  * Mind the PATH here!
@@ -63,6 +50,7 @@ class Discriminator(torch.nn.Module):
 
 def images_to_vectors(images):
     return images.view(-1,784)
+
 def vectors_to_images(vectors):
     return vectors.view(-1,1,28,28)
 
@@ -93,9 +81,8 @@ class Generator(nn.Module):
         x = self.out(x)
         return x
 
-def noise(size):
-    n =  torch.randn(size,100)
-    return n
+def GenerateLatentVector(size):
+    return torch.randn(size,100)
 
 def ones_target(size):
     return torch.ones(size,1)
@@ -103,47 +90,42 @@ def ones_target(size):
 def zeros_target(size):
     return torch.zeros(size,1)
 
-def train_discriminator(optimizer,real_data,fake_data, discriminator, loss):
-    N = real_data.size(0)
+def train_discriminator(optimizer,real_data,latent_data, discriminator, loss):
     optimizer.zero_grad()
 
     prediction_real = discriminator(real_data)
-    error_real = loss(prediction_real,ones_target(N))
+    error_real = loss(prediction_real,ones_target(real_data.size(0)))  #Creates a criterion that measures the Binary Cross Entropy between the target and the input probabilities
     error_real.backward()
 
-    prediction_fake = discriminator(fake_data)
-    error_fake = loss(prediction_fake,zeros_target(N))
+    prediction_fake = discriminator(latent_data)
+    error_fake = loss(prediction_fake,zeros_target(real_data.size(0)))
     error_fake.backward()
 
     optimizer.step()
     return error_real + error_fake, prediction_real, prediction_fake
 
-def train_generator(optimizer,fake_data, discriminator, loss):
-    N = fake_data.size(0)
+def train_generator(optimizer,latent_data, discriminator, loss):
     optimizer.zero_grad()
-    prediction = discriminator(fake_data)
-    error = loss(prediction,ones_target(N))
+    prediction = discriminator(latent_data)
+    error = loss(prediction,ones_target(latent_data.size(0)))
     error.backward()
     optimizer.step()
     return error
 
-def denorm(x):
-    out = (x + 1) / 2
-    return out.clamp(0, 1)
-
-def load_state(generator, discriminator, d_optim, g_optim):
+'''
+def load_learning_state(generator, discriminator, d_optim, g_optim):
     generator_state_dict = torch.load('./results/generator.pth')
     discriminator_state_dict = torch.load('./results/discriminator.pth')
     d_optim_state_dict = torch.load('./results/d_optim.pth')
     g_optim_state_dict = torch.load('./results/g_optim.pth')
     return generator.load_state_dict(generator_state_dict), discriminator.load_state_dict(discriminator_state_dict), d_optim.load_state_dict(d_optim_state_dict), g_optim.load_state_dict(g_optim_state_dict)
 
-def save_state(generator, discriminator, d_optim, g_optim):
+def save_learning_state(generator, discriminator, d_optim, g_optim):
     torch.save(generator.state_dict(), './results/generator.pth')         #save neural network state
     torch.save(discriminator.state_dict(), './results/discriminator.pth')         #save neural network state
     torch.save(d_optim.state_dict(), './results/d_optim.pth')   #save optimizer state
     torch.save(g_optim.state_dict(), './results/g_optim.pth')   #save optimizer state
-
+'''
 
 def main(argv):
 
@@ -162,47 +144,69 @@ def main(argv):
     loss = nn.BCELoss()
 
     num_test_samples = 16
-    test_noise = noise(num_test_samples)
+    test_noise = GenerateLatentVector(num_test_samples)
 
-    load_state(generator, discriminator, d_optim, g_optim)
+    #load_learning_state(generator, discriminator, d_optim, g_optim)
 
-    num_epochs = 139
+    num_epochs = 100
+
+    d_error_list = []; g_error_list = []; epochs_list = []
 
     for epoch in range(num_epochs):
         for n_batch, (real_batch, example_targets) in enumerate(data_loader):
 
             # train the discriminator
             real_data = real_batch.view(-1,784)
+            latent_data = generator(torch.randn(batch_size,100)).detach()
 
-            fake_data = generator(torch.randn(batch_size,100)).detach()
+            d_error,d_pred_real,d_pred_fake = train_discriminator(d_optim,real_data,latent_data, discriminator, loss)
 
-            d_error,d_pred_real,d_pred_fake = train_discriminator(d_optim,real_data,fake_data, discriminator, loss)
 
-            # traing the generator
-            fake_data = generator(noise(batch_size))
-            g_error = train_generator(g_optim,fake_data, discriminator, loss)
+            # train the generator
+            latent_data = generator(GenerateLatentVector(batch_size))
+            g_error = train_generator(g_optim,latent_data, discriminator, loss)
 
-        print("Epoch: ")
+        d_error_list.append(d_error.item())
+        epochs_list.append(epoch)
+        g_error_list.append(g_error.item())
 
-    save_state(generator, discriminator, d_optim, g_optim)
+        if epoch % 20 == 0:
+            print("Epoch: {}/{}".format(epoch, num_epochs))
+            print("d_error (Error_real + error fake Loss)(Discriminator Loss): {}, Generator Loss: {}".format(d_error, g_error))
+            #compare mean of real and fake tensor.  their value should come closer and closer
+            print("D(x): {}, D(G(z)): {}".format(d_pred_real.mean(), d_pred_fake.mean()))
+
+    #save_learning_state(generator, discriminator, d_optim, g_optim)
+
+    #print(d_error_list)
+    #print(g_error_list)
 
     test_images = vectors_to_images(generator(test_noise))
     test_images = test_images.data
 
     fig = plt.figure()
+    title = "Epoch " + str(num_epochs)
+    chart_global_title = fig.suptitle(title)
+    # show generated images
     for i in range(6):
         plt.subplot(2, 3, i+1)
         plt.tight_layout()
         plt.imshow(test_images[i][0], cmap='gray', interpolation = 'none')
-        #plt.imshow(vectors_to_images(fake_data).data, cmap='gray', interpolation = 'none')
-        #plt.imshow(denorm(fake_data[i].reshape((-1, 28,28)).detach())[0], cmap='gray', interpolation = 'none')
-        plt.title("Latent Vector example")
+        #plt.title("Latent Vector example")
         plt.xticks([])          #remove xtick
         plt.yticks([])          #remove ytick
+    plt.show()
+
+    #plot discriminator and adversarial loss over epoches
+    fig = plt.figure()                                                                      #Plot of training and testing error
+    plt.plot(epochs_list, d_error_list, color='blue')
+    plt.plot(epochs_list, g_error_list, color='red')
+    plt.legend(['Discriminator Loss', 'Adversarial Loss'], loc='upper right')
+    plt.xlabel('Epoches')
+    plt.ylabel('Loss')
     plt.show()
 
     return
 
 if __name__ == "__main__":
     main(sys.argv)
-
