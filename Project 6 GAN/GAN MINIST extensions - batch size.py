dbb2a6d9f4f895fb5c77bcb7120bc918d0dfeb32
Casca Kwok <casca.kwok@gmail.com>
2022-05-04 18:59:53 -0700

    Update and rename GAN MINIST extensions batch size.py to GAN MINIST extensions - batch size.py

--- Project 6 GAN/GAN MINIST extensions - batch size.py
+++ Project 6 GAN/GAN MINIST extensions - batch size.py
@@ -1,3 +1,7 @@
+'''
+Wing Man Casca, Kwok
+CS5330 Project 6 - GAN MNIST dataset extensions - Batch size experiment
+'''
 import torch
 from torch import nn, optim
 from torch.autograd.variable import Variable
@@ -8,18 +12,17 @@ import sys
 import matplotlib.pyplot as plt
 import time
 
-
 def mnist_data(batch_size_train):
     #------ Download Training and Testing Dataset.  * Mind the PATH here!
     train_loader = torch.utils.data.DataLoader(
-      torchvision.datasets.MNIST('./dataset/', train=False, download=True,    #download remarked False after downloaded once
+      torchvision.datasets.MNIST('./dataset/', train=False, download=True,   
       transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
-      torchvision.transforms.Normalize((0.5,), (0.5,))])),            #0.1307, 0.3081 = Global mean, Global SD
+      torchvision.transforms.Normalize((0.5,), (0.5,))])),           
       batch_size=batch_size_train, shuffle=False)
 
     return train_loader
 
-class Discriminator(torch.nn.Module):
+class Discriminator(torch.nn.Module):               #define a discriminator NN
     def __init__(self):
         super(Discriminator,self).__init__()
         self.hidden0 = nn.Sequential(
@@ -55,7 +58,7 @@ def images_to_vectors(images):
 def vectors_to_images(vectors):
     return vectors.view(-1,1,28,28)
 
-class Generator(nn.Module):
+class Generator(nn.Module):                 #defines a generator NN
     def __init__(self):
         super(Generator,self).__init__()
         self.hidden0 = nn.Sequential(
@@ -82,13 +85,13 @@ class Generator(nn.Module):
         x = self.out(x)
         return x
 
-def GenerateLatentVector(size):
-    return torch.randn(size,100)    #produces a tensor with gaussian, zero mean, variance 1
+def GenerateLatentVector(size):     #generate a latent vector
+    return torch.randn(size,100)    #randn produces a tensor with gaussian, zero mean, variance 1
 
-def ones_target(size):
+def ones_target(size):              #generate an all 1 tensor
     return torch.ones(size,1)
 
-def zeros_target(size):
+def zeros_target(size):             #generatet a all zero tensor
     return torch.zeros(size,1)
 
 def train_discriminator(optimizer,real_data,latent_data, discriminator, loss):
@@ -113,21 +116,6 @@ def train_generator(optimizer,latent_data, discriminator, loss):
     optimizer.step()
     return error
 
-'''
-def load_learning_state(generator, discriminator, d_optim, g_optim):
-    generator_state_dict = torch.load('./results/generator.pth')
-    discriminator_state_dict = torch.load('./results/discriminator.pth')
-    d_optim_state_dict = torch.load('./results/d_optim.pth')
-    g_optim_state_dict = torch.load('./results/g_optim.pth')
-    return generator.load_state_dict(generator_state_dict), discriminator.load_state_dict(discriminator_state_dict), d_optim.load_state_dict(d_optim_state_dict), g_optim.load_state_dict(g_optim_state_dict)
-
-def save_learning_state(generator, discriminator, d_optim, g_optim):
-    torch.save(generator.state_dict(), './results/generator.pth')         #save neural network state
-    torch.save(discriminator.state_dict(), './results/discriminator.pth')         #save neural network state
-    torch.save(d_optim.state_dict(), './results/d_optim.pth')   #save optimizer state
-    torch.save(g_optim.state_dict(), './results/g_optim.pth')   #save optimizer state
-'''
-
 def main(argv):
 
     torch.manual_seed(42)           #Generate 42 manual seeds and it's "reproducible" (fixed) for every run time
@@ -146,8 +134,6 @@ def main(argv):
 
     num_test_samples = 16
     test_noise = GenerateLatentVector(num_test_samples)
-
-    #load_learning_state(generator, discriminator, d_optim, g_optim)
 
     num_epochs = 20
 
@@ -171,26 +157,23 @@ def main(argv):
         epochs_list.append(epoch)
         g_error_list.append(g_error.item())
 
-        if epoch % 20 == 0:
+        if epoch % 20 == 0:             #check status of every 20 epochs
             print("Epoch: {}/{}".format(epoch, num_epochs))
             print("d_error (Error_real + error fake Loss)(Discriminator Loss): {}, Generator Loss: {}".format(d_error, g_error))
             #compare mean of real and fake tensor.  their value should come closer and closer
             print("D(x): {}, D(G(z)): {}".format(d_pred_real.mean(), d_pred_fake.mean()))
 
-    #save_learning_state(generator, discriminator, d_optim, g_optim)
-
-    #print(d_error_list)
-    #print(g_error_list)
 
     print("Run time (in second)", time.time() - start_time)
 
     test_images = vectors_to_images(generator(test_noise))
     test_images = test_images.data
 
+    # show generated images
     fig = plt.figure()
     title = "Epoch " + str(num_epochs)
     chart_global_title = fig.suptitle(title)
-    # show generated images
+   
     for i in range(6):
         plt.subplot(2, 3, i+1)
         plt.tight_layout()
@@ -205,6 +188,7 @@ def main(argv):
     plt.plot(epochs_list, d_error_list, color='blue')
     plt.plot(epochs_list, g_error_list, color='red')
     plt.legend(['Discriminator Loss', 'Adversarial Loss'], loc='upper right')
+    plt.title("Plot of Loss Function against Epochs")
     plt.xlabel('Epoches')
     plt.ylabel('Loss')
     plt.show()
