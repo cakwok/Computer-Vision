889ca04b7d078c18d8a062ec81182ce2203ee2c7
Casca Kwok <casca.kwok@gmail.com>
2022-05-29 23:59:45 -0700

    Update filter.h

--- Project1 - Real Time Video Filtering/filter.h
+++ Project1 - Real Time Video Filtering/filter.h
@@ -11,13 +11,16 @@
 using namespace cv;
 using namespace std;
 
-int greyscale( cv::Mat &img, cv::Mat &grey_image );
-int blur5x5( cv::Mat &img, cv::Mat &blur_16bit );
+int greyscale(Mat &img, Mat &grey_image );
+int blur5x5(Mat &img, Mat &blur_16bit );
 int sobelX3x3(Mat &img, Mat &sobelx_16bit );
-int sobelY3x3(Mat &img, Mat &sobelx_16bit );
-int magnitude(Mat &sobelx_16bit, Mat &sobely_16bit, Mat &gradientmag_16bit );
+int sobelY3x3(Mat &img, Mat &sobely_16bit );
+int magnitude(Mat &sobelx_16bit, Mat &sobely_16bit, Mat &gradientmag_8bit );
 int blurQuantize(Mat &blur_16bit, Mat &quantize_16bit, int levels);
 int cartoon(Mat &quantize_16bit, Mat &gradientmag_16bit, Mat &cartoon_16bit, int levels, int magThreshold );
 int contrast(Mat &img, Mat &contrast_16bit );
 int mirror(Mat &img, Mat &mirror_8bit );
+int meme(Mat &img, string &text);
+int blending(Mat &img, Mat &dst);
+
 #endif /* FILTER_H */
